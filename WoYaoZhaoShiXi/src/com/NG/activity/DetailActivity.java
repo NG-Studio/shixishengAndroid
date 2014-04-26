a52a60052fbeb9b3ba6977dd09ee1bae8767ff7a
Jiyuan-Mac <jiyuan0371@sina.com>
2014-04-26 18:28:19 +0800

    与立杭share联合ok 出现一个dismiss的bug

--- WoYaoZhaoShiXi/src/com/NG/activity/DetailActivity.java
+++ WoYaoZhaoShiXi/src/com/NG/activity/DetailActivity.java
@@ -1,6 +1,5 @@
 package com.NG.activity;
 
-<<<<<<< Updated upstream
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.HashMap;
@@ -10,13 +9,12 @@ import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.framework.utils.UIHandler;
 import cn.sharesdk.onekeyshare.OnekeyShare;
-=======
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
->>>>>>> Stashed changes
 
 import com.NG.entity.ItemDetail;
 import com.NG.entity.MessageDetail;
@@ -37,12 +35,10 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-<<<<<<< Updated upstream
 import android.os.Environment;
 import android.os.Handler.Callback;
-=======
 import android.os.Handler;
->>>>>>> Stashed changes
+
 import android.os.Message;
 import android.text.Html;
 import android.util.Log;
@@ -52,30 +48,27 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-<<<<<<< Updated upstream
+
 public class DetailActivity extends Activity implements PlatformActionListener, Callback{
-=======
-public class DetailActivity extends Activity{
+
 	final static String TAG = "DetailActivity";
 	
->>>>>>> Stashed changes
+
 	private TextView titleView;
 	private TextView timeView;
 	private TextView sourceView;
 	private TextView contentView;
 	
-<<<<<<< Updated upstream
+
 	private Button b_collect;
 	//private Button b_share;
 	
-=======
 	private ProgressDialog proDialog;
 	
 	private ItemDetail mItem;
 	private DetailInfoLoader mItemLoader;
 	
 	private String url;
->>>>>>> Stashed changes
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +76,6 @@ public class DetailActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
 		
-<<<<<<< Updated upstream
 		// 初始化shareSDK
 		ShareSDK.initSDK(this);
 		
@@ -106,14 +98,13 @@ public class DetailActivity extends Activity{
 				toast.show();
 			}
 		});
-=======
+
 		Bundle bundle = getIntent().getExtras();
 		int item_id = bundle.getInt("item_id");
 		url = "http://51zhaoshixi.com:8008/info/get_item?item_id=" + item_id;
 		initView();
 		
 		//loadData();
->>>>>>> Stashed changes
 		
 		mItemLoader = new DetailInfoLoader();
 		
@@ -182,7 +173,6 @@ public class DetailActivity extends Activity{
 		
 	}
 
-<<<<<<< Updated upstream
 	private static final int MSG_TOAST = 1;
 	private static final int MSG_ACTION_CCALLBACK = 2;
 	private static final int MSG_CANCEL_NOTIFY = 3;
@@ -344,7 +334,7 @@ public class DetailActivity extends Activity{
 			e.printStackTrace();
 		}
 	}
-=======
+	
 	private Handler mHandler = new Handler() {
 		@Override
 		public void handleMessage(Message message) {
@@ -356,6 +346,7 @@ public class DetailActivity extends Activity{
 			} catch (Exception e) {
 				// TODO: handle exception
 				System.out.println("有些没有");
+				proDialog.dismiss();
 			}
 		}
 	};
@@ -383,6 +374,4 @@ public class DetailActivity extends Activity{
 	}
 	
 
-
->>>>>>> Stashed changes
 }

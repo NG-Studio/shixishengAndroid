abfd57ff343a6dcc81fed357b98db8d9a0e05ecd
hustcalm <licalmer@gmail.com>
2014-05-23 00:18:05 +0800

    替换splash图片，显示的时候全屏

--- WoYaoZhaoShiXi/src/com/NG/activity/SplashActivity.java
+++ WoYaoZhaoShiXi/src/com/NG/activity/SplashActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity{
 	private final int SPLASH_DISPLAY_LENGHT = 2000; // Splash display time in ms
@@ -31,6 +33,10 @@ public class SplashActivity extends Activity{
         System.out.println(displayWidth);
         System.out.println(density);
         
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.splash);
         
         new Handler().postDelayed(new Runnable() {

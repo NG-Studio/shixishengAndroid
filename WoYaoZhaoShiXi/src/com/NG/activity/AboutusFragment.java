214aa0aeb0106c5c00039764f1fdb0c70e006256
Jiyuan <jiyuan0371@sina.com>
2014-06-10 22:11:48 +0800

    add checkout the version and can download App of new version

--- WoYaoZhaoShiXi/src/com/NG/activity/AboutusFragment.java
+++ WoYaoZhaoShiXi/src/com/NG/activity/AboutusFragment.java
@@ -1,35 +1,56 @@
 package com.NG.activity;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
-
+import com.NG.loader.DownLoadManager;
+import com.NG.loader.UpdataInfo;
+import com.NG.loader.UpdataInfoParser;
 import com.ngstudio.zhaoshixi.R;
 
 public class AboutusFragment extends Fragment {
 
+	private final String TAG = "AboutusFragment";
+	
+	private Activity mActivity;
 	private Context mContext;
 	private Button checkVersionBth;
 	
-	private String httpUrl = "http://211.155.86.159/online/info/check_version";
-
+	private final int UPDATA_NONEED = 0;
+	private final int UPDATA_CLIENT = 1;
+	private final int GET_UNDATAINFO_ERROR = 2;
+	private final int SDCARD_NOMOUNTED = 3;
+	private final int DOWN_ERROR = 4;
+	
+	private UpdataInfo info;
+	private String localVersion;
+	
+	//private String httpUrl = "http://211.155.86.159/online/info/check_version";
+	private String httpUrl = "http://washixi.com/latest/update.xml";
 	public AboutusFragment() {
 
 	}
@@ -37,6 +58,7 @@ public class AboutusFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		mActivity = this.getActivity();
 		mContext = this.getActivity().getApplicationContext();
 		
 
@@ -49,58 +71,189 @@ public class AboutusFragment extends Fragment {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				/*
-				String resultText = "";
-
-				HttpGet httpRequest = new HttpGet(httpUrl);
-				// 取得HttpClient对象
-				HttpClient httpclient = new DefaultHttpClient();
-				// 请求HttpClient，取得HttpResponse
-				HttpResponse httpResponse;
 				try {
-					httpResponse = httpclient.execute(httpRequest);
-
-					// 请求成功
-					if (httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-						// 取得返回的字符串
-						String strResult = EntityUtils.toString(httpResponse
-								.getEntity());
-						resultText = strResult;
-					} else {
-						resultText = "请求错误!";
-					}
-				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				*/
-				try {
-					
 					Toast.makeText(mContext, "当前版本为"+getVersionName(), Toast.LENGTH_SHORT).show();
 					
+					CheckVersionTask cv = new CheckVersionTask();
+					new Thread(cv).start();
+				
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-
 			}
 
 		});
+		
+		
 
 		return rootView;
 	}
-
+	
+	
+	/*
+	 * 获取当前程序的版本号
+	 */
 	private String getVersionName() throws Exception {
 		// 获取packagemanager的实例
 		PackageManager packageManager = mContext.getPackageManager();
 		// getPackageName()是你当前类的包名，0代表是获取版本信息
-		PackageInfo packInfo = packageManager.getPackageInfo(
-				mContext.getPackageName(), 0);
-		String version = packInfo.versionName;
-		return version;
+		PackageInfo packInfo = packageManager.getPackageInfo(mContext.getPackageName(),
+				0);
+		return packInfo.versionName;
 	}
+
+	/*
+	 * 从服务器获取xml解析并进行比对版本号
+	 */
+	public class CheckVersionTask implements Runnable {
+
+		public void run() {
+			try {
+				// 从资源文件获取服务器 地址
+				String path = httpUrl;
+				// 包装成url的对象
+				URL url = new URL(path);
+				HttpURLConnection conn = (HttpURLConnection) url
+						.openConnection();
+				conn.setConnectTimeout(5000);
+				InputStream is = conn.getInputStream();
+				info = UpdataInfoParser.getUpdataInfo(is);
+				System.out
+						.println("VersionActivity            ----------->          info = "
+								+ info);
+				if (info.getVersion().equals(localVersion)) {
+					Log.i(TAG, "版本号相同无需升级");
+					Message msg = new Message();
+					msg.what = UPDATA_NONEED;
+					handler.sendMessage(msg);
+					// LoginMain();
+				} else {
+					Log.i(TAG, "版本号不同 ,提示用户升级 ");
+					Message msg = new Message();
+					msg.what = UPDATA_CLIENT;
+					handler.sendMessage(msg);
+				}
+			} catch (Exception e) {
+				// 待处理
+				Message msg = new Message();
+				msg.what = GET_UNDATAINFO_ERROR;
+				handler.sendMessage(msg);
+				e.printStackTrace();
+			}
+		}
+	}
+
+	Handler handler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			switch (msg.what) {
+			case UPDATA_NONEED:
+				Toast.makeText(mContext.getApplicationContext(), "版本号相同无需升级",
+						Toast.LENGTH_SHORT).show();
+			case UPDATA_CLIENT:
+				// 对话框通知用户升级程序
+				// Toast.makeText(getApplicationContext(), "可以升级程序啦~",
+				// 1).show();
+				showUpdataDialog();
+				break;
+			case GET_UNDATAINFO_ERROR:
+				// 服务器超时
+				Toast.makeText(mContext.getApplicationContext(), "获取服务器更新信息失败", 1)
+						.show();
+				// LoginMain();
+				break;
+			case SDCARD_NOMOUNTED:
+				// sdcard不可用
+				Toast.makeText(mContext.getApplicationContext(), "SD卡不可用",1).show();
+				break;
+			case DOWN_ERROR:
+				// 下载apk失败
+				Toast.makeText(mContext.getApplicationContext(), "下载新版本失败", 1).show();
+				// LoginMain();
+				break;
+			}
+		}
+	};
+
+	/*
+	 * 
+	 * 弹出对话框通知用户更新程序
+	 * 
+	 * 弹出对话框的步骤： 1.创建alertDialog的builder. 2.要给builder设置属性, 对话框的内容,样式,按钮
+	 * 3.通过builder 创建一个对话框 4.对话框show()出来
+	 */
+	protected void showUpdataDialog() {
+		AlertDialog.Builder builer = new Builder(mActivity);
+		builer.setTitle("版本升级");
+		builer.setMessage(info.getDescription());
+		// 当点确定按钮时从服务器上下载 新的apk 然后安装
+		builer.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				Log.i(TAG, "下载apk,更新");
+				downLoadApk();
+			}
+		});
+		// 当点取消按钮时进行登录
+		builer.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				// LoginMain();
+			}
+		});
+		AlertDialog dialog = builer.create();
+		dialog.show();
+	}
+
+	/*
+	 * 从服务器中下载APK
+	 */
+	protected void downLoadApk() {
+		final ProgressDialog pd; // 进度条对话框
+		pd = new ProgressDialog(mActivity);
+		pd.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		pd.setMessage("正在下载更新");
+		if (!Environment.getExternalStorageState().equals(
+				Environment.MEDIA_MOUNTED)) {
+			Message msg = new Message();
+			msg.what = SDCARD_NOMOUNTED;
+			handler.sendMessage(msg);
+		} else {
+			pd.show();
+			new Thread() {
+				@Override
+				public void run() {
+					try {
+						File file = DownLoadManager.getFileFromServer(
+								info.getUrl(), pd);
+						sleep(1000);
+						installApk(file);
+						pd.dismiss(); // 结束掉进度条对话框
+
+					} catch (Exception e) {
+						Message msg = new Message();
+						msg.what = DOWN_ERROR;
+						handler.sendMessage(msg);
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+	}
+
+	// 安装apk
+	protected void installApk(File file) {
+		Intent intent = new Intent();
+		// 执行动作
+		intent.setAction(Intent.ACTION_VIEW);
+		// 执行的数据类型
+		intent.setDataAndType(Uri.fromFile(file),
+				"application/vnd.android.package-archive");
+		startActivity(intent);
+	}
+	
 
 }
